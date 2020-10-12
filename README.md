5d0d735ec7563f167854011e3544401b56c9a161
Your Name <you@example.com>
2020-10-12 14:56:25 +0000

    message plugin

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # [Moodle Plugin Tutorial](https://www.youtube.com/watch?v=r8XDrQv_YYM&list=PLnNniujrnp0mFwUNszRcI2OBCiBAh9Iqs&index=2)
+## Local Mesaage
+### Functionality
+- Form for admins to add new notifications
+- Store read messages for users, do not diplay twice
+- Store notifications in database
--- manage.php
+++ manage.php
@@ -18,11 +18,25 @@
  * @package     local_message
  * @author      Kristian
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @var stdClass $plugin
  */
 
-defined('MOODLE_INTERNAL') || die();
+require_once(__DIR__ . '/../../config.php');
 
-$plugin->component = 'local_message';
-$plugin->version = 2020071900;
-$plugin->requires = 2016052300; // Moodle version
+global $DB;
+
+$PAGE->set_url(new moodle_url('/local/message/manage.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title('Manage messages');
+
+$messages = $DB->get_records('local_message');
+
+echo $OUTPUT->header();
+$templatecontext = (object)[
+    'messages' => array_values($messages),
+    'editurl' => new moodle_url('/local/message/edit.php'),
+];
+
+
+//echo $OUTPUT->render_from_template('local_message/manage', $templatecontext);
+echo $OUTPUT->render_from_template('local_message/manage', $templatecontext);
+echo $OUTPUT->footer();
